d375f7109f4f995bed8d7f8d079ef89dffd10e95
ioana.girba@principal33.com <ioana.girba@ITRO-166.local>
2024-03-11 16:02:27 +0200

    done stylinh header

---
+++ GoogleClone/src/components/SearchInput.tsx
@@ -0,0 +1,5 @@
+function SearchInput() {
+  return <div>SearchInput</div>;
+}
+
+export default SearchInput;
--- GoogleClone/src/components/StyledHomeHeader.tsx
+++
@@ -1,8 +0,0 @@
-import { styled } from "@mui/system";
-
-export const StyledHomeHeader = styled("div")``;
-export const StyledHeaderWrapper = styled("div")``;
-export const StyledHeaderLeft = styled("div")``;
-
-export const StyledHeaderRight = styled("div")``;
-export const StyledLogoContainer = styled("div")``;
