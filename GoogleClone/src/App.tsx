4adb60ec6e643516d8406b3ae344714071cca5bf
ioana.girba@principal33.com <ioana.girba@ITRO-166.local>
2024-03-11 12:40:57 +0200

    instaling necessary tools

--- GoogleClone/src/App.tsx
+++ GoogleClone/src/App.tsx
@@ -1,9 +1,15 @@
-
-function App() {
+import { Home } from "@mui/icons-material"
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+const App = () => {
   
   return (
     <>
-      <div>Google clone</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
     </>
   )
 }
