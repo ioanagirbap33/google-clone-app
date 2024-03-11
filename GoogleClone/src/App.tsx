35501c104d942ec5ea3c593f8df8b143b57bc56c
ioana.girba@principal33.com <ioana.girba@ITRO-166.local>
2024-03-11 12:48:52 +0200

    add home page

--- GoogleClone/src/App.tsx
+++ GoogleClone/src/App.tsx
@@ -1,6 +1,5 @@
-import { Home } from "@mui/icons-material"
-import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
 const App = () => {
   
   return (
