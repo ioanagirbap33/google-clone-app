e74bf1ca872cfcad02c41d9a2e2a4e6dcb2ba5b5
ioana.girba@principal33.com <ioana.girba@ITRO-166.local>
2024-03-11 12:56:21 +0200

    change folder

--- GoogleClone/src/App.tsx
+++ GoogleClone/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./components/Home"
+import Home from "./pages/Home"
+import SearchPage from "./pages/SearchPage"
 const App = () => {
   
   return (
@@ -7,6 +8,8 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/search" element={<SearchPage />}/>
+
       </Routes>
     </Router>
     </>
