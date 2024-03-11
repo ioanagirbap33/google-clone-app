87a1955eeb8c45916112f74446633e9fa9e1fc64
ioana.girba@principal33.com <ioana.girba@ITRO-166.local>
2024-03-11 16:47:06 +0200

    style search component

--- GoogleClone/src/components/SearchInput.Style.tsx
+++ GoogleClone/src/components/SearchInput.Style.tsx
@@ -1,1 +1,56 @@
 import { styled } from "@mui/system";
+import SearchIcon from "@mui/icons-material/Search";
+import MicIcon from "@mui/icons-material/Mic";
+
+export const StyledInputWrapper = styled("div")`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 75%;
+  height: 20px;
+  padding: 16px 24px;
+  margin: 0 auto;
+  margin-top: 24px;
+  max-width: 560px;
+  border: 0.1px solid rgb(95, 99, 104);
+  border-radius: 999px;
+  input {
+    flex: 1;
+    background-color: rgba(32, 33, 36, 1);
+    border: none;
+    padding: 16px 24px;
+    font-size: medium;
+    &:focus {
+      outline-width: 0;
+    }
+  }
+`;
+export const StyledSearchIcon = styled(SearchIcon)`
+  cursor: pointer;
+  color: #9aa0a6;
+  font-size: 1.5rem;
+`;
+export const StyledMicIcon = styled(MicIcon)`
+  cursor: pointer;
+  color: #9aa0a6;
+  font-size: 1.5rem;
+`;
+export const StyledButtonContainer = styled("div")`
+  display: flex;
+  justify-content: center;
+  padding-top: 18px;
+  button {
+    min-height: 32px;
+    background-color: #303134;
+    color: #e8eaed;
+    border: 1px solid #303134;
+    border-radius: 4px;
+    padding: 0 16px;
+    margin: 11px 4px;
+    cursor: pointer;
+    &:hover {
+      border: 1px solid lightgray;
+      border-radius: 4px;
+    }
+  }
+`;
--- GoogleClone/src/components/SearchInput.tsx
+++ GoogleClone/src/components/SearchInput.tsx
@@ -1,5 +1,27 @@
+import { Tooltip } from "@mui/material";
+import {
+  StyledInputWrapper,
+  StyledSearchIcon,
+  StyledMicIcon,
+  StyledButtonContainer,
+} from "./SearchInput.Style";
+// import { Button } from "@mui/material";
 function SearchInput() {
-  return <div>SearchInput</div>;
+  return (
+    <form>
+      <StyledInputWrapper>
+        <StyledSearchIcon />
+        <input />
+        <Tooltip title="Search by voice">
+          <StyledMicIcon />
+        </Tooltip>
+      </StyledInputWrapper>
+      <StyledButtonContainer>
+        <button type="submit">Google Search</button>
+        <button type="submit">I'm feeling lucky</button>
+      </StyledButtonContainer>
+    </form>
+  );
 }
 
 export default SearchInput;
